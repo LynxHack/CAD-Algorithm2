e589aa083b492164bb860124947de79532f6ddb1
Lynx Lu <kevinlynx_l@hotmail.com>
2020-02-10 17:27:18 -0800

    Cleaned up documentation

--- hw2.py
+++ hw2.py
@@ -9,6 +9,10 @@ import random
 from colors import COLORS
 from collections import deque
 
+import random
+import numpy as np
+import copy
+
 try:
     # for Python2
     from Tkinter import *   ## notice capitalized T in Tkinter 
@@ -36,14 +40,14 @@ def parseFile(filename):
             if net == '':
                 continue
             nets.append(list(map(lambda x: int(x), net.split(' '))))
-        # print(nets, numcells, numconn, numrows, numcols)
         return nets, numcells, numconn, numrows, numcols
 
-# ## Takes in a coordinate, color, and the existing canvas to draw a color in the grid
+## Takes in a coordinate, color, and the existing canvas to draw a color in the grid
 def drawcell(i, j, color, c):
     # print(sizex*i,sizey*j)
     c.create_rectangle(sizex*i,sizey*j,sizex*(i+1),sizey*(j+1), fill=color)
 
+## Draw a line between two coordinates
 def drawline(i1, j1, i2, j2, c):
     x1 = sizey * (2 * i1 + 1) / 2
     x2 = sizey * (2 * i2 + 1) / 2
@@ -52,14 +56,17 @@ def drawline(i1, j1, i2, j2, c):
     print(x1, x2, y1, y2)
     c.create_line(x1, y1, x2, y2, fill='black')
 
-# ## draw the updated grid colors
+## draw the updated grid colors
 def updategrid(grid, c):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             value = grid[i][j]
             colorid = value % len(COLORS)
+            if colorid == 0 and value > len(COLORS):
+                colorid += 1
             drawcell(i, j, COLORS[colorid], c)
 
+# Draw the connected lines between each cell (not used to prevent overhead)
 def updateconn(conn, loc, c):
     for i in range(len(conn)):
         numconn = conn[i][0]
@@ -69,29 +76,6 @@ def updateconn(conn, loc, c):
             end = conn[i][j]
             print(conn[i], start, end)
             drawline(loc[start][0], loc[start][1], loc[end][0], loc[end][1], c)
-
-# filename = sys.argv[1]
-filename = "alu2"
-nets, numcells, numconn, numrows, numcols = parseFile("./ass2_files/"+filename+".txt")
-
-root = Tk()
-
-grid = [[0 for x in range(numcols)] for y in range(numrows)]
-
-sizex = 1000/numcols
-sizey = 500/numrows
-locations = [0] * (numcells)
-
-
-# set up white grids
-frame = Frame(root, width=1000, height=1000)
-frame.pack()
-c = Canvas(frame, bg='white', width=1000, height=1000)
-c.focus_set()
-c.pack()
-
-# Run algorithm in background via a thread
-updategrid(grid, c)
 
 # Calculate the half perim of one net
 def halfperim(net, loc):
@@ -104,13 +88,11 @@ def halfperim(net, loc):
     for i in range(1, len(net)):
         coordx = loc[net[i]][0] 
         coordy = loc[net[i]][1]
-        # print(coordx, coordy, minx, maxx, miny, maxy)
         minx = min([coordx, minx])
         maxx = max([coordx, maxx])
         miny = min([coordy, miny])
         maxy = max([coordy, maxy])
 
-    ## afterwards, return the final cost of half perimeter
     return (maxx - minx) + (maxy - miny)
 
 ## goes through all the nets and calculate the sum of cost
@@ -120,35 +102,50 @@ def getcost(nets, loc):
         sum += halfperim(nets[i], loc)
     return sum
 
+## Grab a random coordinate from the grid
+def getOneCoord(grid):
+    i1 = random.sample(range(0, len(grid)), 1)[0]
+    j1 = random.sample(range(0, len(grid[0])), 1)[0]
+    return [i1, j1]
+
+## Grab two random coordinates from the grid
+def getTwoCoords(grid):
+    i1 = random.sample(range(0, len(grid)), 1)[0]
+    i2 = random.sample(range(0, len(grid)), 1)[0]
+    j1 = 0
+    j2 = 0
+
+    ## if same row is picked make sure two distinct columns are picked
+    if i1 == i2:
+        cols = random.sample(range(0, len(grid[0])), 2)
+        j1 = cols[0]
+        j2 = cols[1]
+    ## Otherwise pick any other two columns
+    else:
+        j1 = random.sample(range(0, len(grid[0])), 1)[0]
+        j2 = random.sample(range(0, len(grid[0])), 1)[0]
+    return [[i1, j1], [i2, j2]]
+
 ## First randomize location of all placements (numcells), 
-# can just place them all together for now
 def initplacement(grid):
-    for k in range(numcells):
-        row = k % len(grid[0])
-        col = int((k - row) / len(grid[0]))
-        grid[col][row] = k
-        locations[k] = [col, row]
+    indices = random.sample(range(0, len(grid) * len(grid[0])), numcells)
+    for k in range(len(indices)):
+        i = int(indices[k] / len(grid[0]))
+        j = int(indices[k] % len(grid[0]))
+        grid[i][j] = k
+        locations[k] = [i, j]
     return locations
 
-def getTwoCoords(grid):
-    indices = random.sample(range(0, len(grid) * len(grid[0])), 2)
-    j1 = int(indices[0] % len(grid[0]))
-    i1 = int((indices[0] - j1) / len(grid))
-    j2 = int(indices[1] % len(grid[1]))
-    i2 = int((indices[1] - j2) / len(grid))
-    return [[i1, j1], [i2, j2]]
-## Goes through sim anneal
-import random
-import numpy as np
-import copy
+## Go through sim anneal iterations
 def simAnneal(grid, nets, c):
     ## first place the grid and get the locations
     loc = initplacement(grid)
     ## temperature is between 0 and 1 non inclusive
-    temp = 0.6
+    temp = numcells
+    # t0 = temp
     ## rate at which temp decreases, lower is faster
-    alpha = 0.8
-    # k = 0
+    alpha = 0.999
+    # k = 1
 
     num_noswaps = 0
     currcost = getcost(nets, loc)
@@ -156,87 +153,109 @@ def simAnneal(grid, nets, c):
     ## Iterate until end condition
     while True:
         # end condition
-        if num_noswaps > 10:
-            print("Final cost: ", currcost)
-            return currcost
+        if num_noswaps > 1000:
+            break
 
-        ## Choose two random cells to swap -> ex. [23, 56] TODO, fix this
-        # toswap = random.sample(range(1, numcells), 2)
+        ## Choose two random cells to swap
         coords = getTwoCoords(grid)
         
         samp1 = coords[0]
         samp2 = coords[1]
+
+        print("Cost: ", currcost, "Temp: ", temp, num_noswaps)
         toswap = [0, 0]
-
-        ## check if swapping two cells, or just a cell with an empty
-        bothcells = True
-
-        if grid[samp1[0]][samp1[1]] == 0 and grid[samp2[0]][samp2[1]] == 0:
+        toswap[0] = grid[samp1[0]][samp1[1]]
+        toswap[1] = grid[samp2[0]][samp2[1]]
+        
+        ## redo if both are just blank cells
+        if toswap[0] == 0 and toswap[1] == 0:
             continue
-        elif grid[samp1[0]][samp1[1]] != 0 and grid[samp2[0]][samp2[1]] != 0:
-            toswap[0] = grid[samp1[0]][samp1[1]]
-            toswap[1] = grid[samp2[0]][samp2[1]]
-            t = loc[toswap[0]]
-            loc[toswap[0]] = loc[toswap[1]]
-            loc[toswap[1]] = t
-        elif 
+
+        ## perform swap of coordinates
+        loc[toswap[0]] = samp2
+        loc[toswap[1]] = samp1
+
         ## calculate cost delta, if new placement - old, do a swap first, 
         # swap back later if needed
-        # newloc = copy.deepcopy(loc)
-        # t = loc[toswap[0]]
-        # loc[toswap[0]] = loc[toswap[1]]
-        # loc[toswap[1]] = t
-
         newcost = getcost(nets, loc)
         deltac = newcost - currcost
         r = random.random()
 
         ## See if should swap
         if r < np.exp(-deltac/temp):
-            # loc = newloc
             ## swap it in the grid officially
-            pt1 = loc[toswap[0]]
-            pt2 = loc[toswap[1]]
-            print(pt1, pt2)
-            t = grid[pt1[0]][pt1[1]]
-            grid[pt1[0]][pt1[1]] = grid[pt2[0]][pt2[1]]
-            grid[pt2[0]][pt2[1]] = t
+            t = grid[samp1[0]][samp1[1]]
+            grid[samp1[0]][samp1[1]] = grid[samp2[0]][samp2[1]]
+            grid[samp2[0]][samp2[1]] = t
 
             ## rerender the graphics just for the two cells, don't need to rerender everything
-            drawcell(pt1[0], pt1[1], COLORS[toswap[1]], c)
-            drawcell(pt2[0], pt2[1], COLORS[toswap[0]], c)
-            # updategrid(grid, c)
+            color1 = toswap[1] % len(COLORS)
+            color2 = toswap[0] % len(COLORS)
+            if color1 == 0 and toswap[1] > len(COLORS):
+                color1 += 1
+            if color2 == 1 and toswap[0] > len(COLORS):
+                color2 += 1
+            if graphicsEnabled:
+                drawcell(samp1[0], samp1[1], COLORS[color1], c)
+                drawcell(samp2[0], samp2[1], COLORS[color2], c)
 
-            currcost = newcost
-            num_noswaps = 0
-        else:
-            ## swap assignment back, don't want this wap
-            if bothcells:
-                t = loc[toswap[0]]
-                loc[toswap[0]] = loc[toswap[1]]
-                loc[toswap[1]] = t
-                num_noswaps += 1
+            ## update the new cost
+            if newcost != currcost:
+                num_noswaps = 0
+                currcost = newcost
             else:
+                num_noswaps += 1
+        else:
+            ## swap assignment back, don't want this swap
+            loc[toswap[0]] = samp1
+            loc[toswap[1]] = samp2
+            num_noswaps += 1
 
-        
         ## reduce temperature via constant scaling (Oldenburg)
         temp *= alpha
 
+        ## reduce temperature via inverse log schedule
+        # temp = temp / math.log(k+1)
+        
+        ## reduce via exponential decay
+        # temp = math.exp(-alpha*k) * t0
+        # k += 1
+    print("Final cost: ", currcost)
+    return currcost
+
+## Main function
+if __name__== "__main__":
+    filename = sys.argv[1]
+    graphicsEnabled =  True
+    if len(sys.argv) > 2:
+        graphicsEnabled = sys.argv[2]
+
+    nets, numcells, numconn, numrows, numcols = parseFile("./ass2_files/"+filename+".txt")
+
+    root = Tk()
+
+    grid = [[0 for x in range(numcols)] for y in range(numrows)]
+    sizex = 1000/numcols
+    sizey = 500/numrows
+    locations = [0] * (numcells)
 
 
-        ## reduce temperature exponential decay (to be tested)
-        ## temp = temp / log(k+1)
-        ## k += 1
+    # set up white grids
+    frame = Frame(root, width=1000, height=1000)
+    frame.pack()
+    c = Canvas(frame, bg='white', width=1000, height=1000)
+    c.focus_set()
+    c.pack()
 
-# result = simAnneal(grid, nets)
-# updateconn(nets, locations, c)
+    # Run algorithm in background via a thread
+    updategrid(grid, c)
 
-thread = threading.Thread(target = simAnneal, args = (grid, nets, c))
-thread.start()
+    thread = threading.Thread(target = simAnneal, args = (grid, nets, c))
+    thread.start()
 
-# simAnneal(grid)
-# print(locations)
-updategrid(grid, c)
-root.mainloop()
+    # simAnneal(grid)
+    # print(locations)
+    updategrid(grid, c)
+    root.mainloop()
 
 
